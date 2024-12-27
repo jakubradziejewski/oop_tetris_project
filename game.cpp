7214adeab53c617d3967aa1551e66bf39337c7f6
Jakub Radziejewski <150089992+jakubradziejewski@users.noreply.github.com>
2024-12-27 16:43:19 +0100

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 #include "game.h"
 #include "block.h"
 #include "blocks.cpp"
+#include <memory>
 #include <random>
 #include "position.h"
 #include "grid.h"
@@ -15,38 +16,42 @@ Game::Game() {
     level = 1;
 }
 
-Block Game::GetRandomBlock() {
+std::unique_ptr<BlockBase> Game::GetRandomBlock() {
     if (blocks.empty()) {
         blocks = GetAllBlocks();
     }
     int randomIndex = rand() % blocks.size();
-    // uses Block class - dependency
-    Block block = blocks[randomIndex];
+    std::unique_ptr<BlockBase> block = std::move(blocks[randomIndex]);
     blocks.erase(blocks.begin() + randomIndex);
     return block;
 }
 
-std::vector<Block> Game::GetAllBlocks() {
-    return {IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()};
-
+std::vector<std::unique_ptr<BlockBase>> Game::GetAllBlocks() {
+    std::vector<std::unique_ptr<BlockBase>> newBlocks;
+    newBlocks.push_back(std::make_unique<IBlock>());
+    newBlocks.push_back(std::make_unique<JBlock>());
+    newBlocks.push_back(std::make_unique<LBlock>());
+    newBlocks.push_back(std::make_unique<OBlock>());
+    newBlocks.push_back(std::make_unique<SBlock>());
+    newBlocks.push_back(std::make_unique<TBlock>());
+    newBlocks.push_back(std::make_unique<ZBlock>());
+    return newBlocks;
 }
 
 void Game::Draw() {
     grid.Draw();
-    currentBlock.Draw(11, 11);
-    switch (nextBlock.id) {
+    currentBlock->Draw(11, 11);
+    switch (nextBlock->id) {
         case 3:
-            nextBlock.Draw(255, 370);
+            nextBlock->Draw(255, 370);
             break;
         case 4:
-            nextBlock.Draw(255, 360);
+            nextBlock->Draw(255, 360);
             break;
         default:
-            nextBlock.Draw(270, 350);
+            nextBlock->Draw(270, 350);
             break;
     }
-
-
 }
 
 void Game::HandleInput() {
@@ -68,40 +73,39 @@ void Game::HandleInput() {
         case KEY_UP:
             RotateBlock();
             break;
-
     }
 }
 
 void Game::MoveBlockLeft() {
     if (!gameOver) {
-        currentBlock.Move(0, -1);
+        currentBlock->Move(0, -1);
         if (IsBlockOutside() || !BlockFits()) {
-            currentBlock.Move(0, 1);
+            currentBlock->Move(0, 1);
         }
     }
 }
 
 void Game::MoveBlockRight() {
     if (!gameOver) {
-        currentBlock.Move(0, 1);
+        currentBlock->Move(0, 1);
         if (IsBlockOutside() || !BlockFits()) {
-            currentBlock.Move(0, -1);
+            currentBlock->Move(0, -1);
         }
     }
 }
 
 void Game::MoveBlockDown() {
     if (!gameOver) {
-        currentBlock.Move(1, 0);
+        currentBlock->Move(1, 0);
         if (IsBlockOutside() || !BlockFits()) {
-            currentBlock.Move(-1, 0);
+            currentBlock->Move(-1, 0);
             LockBlock();
         }
     }
 }
 
 bool Game::IsBlockOutside() {
-    std::vector<Position> tiles = currentBlock.GetCellPosition();
+    std::vector<Position> tiles = currentBlock->GetCellPosition();
     for (Position item: tiles) {
         if (grid.IsCellOutside(item.row, item.column)) {
             return true;
@@ -112,48 +116,37 @@ bool Game::IsBlockOutside() {
 
 void Game::RotateBlock() {
     if (!gameOver) {
-
-        currentBlock.Rotate();
+        currentBlock->Rotate();
         if (IsBlockOutside() || !BlockFits()) {
-            currentBlock.UndoRotation();
+            currentBlock->UndoRotation();
         }
     }
 }
 
 void Game::LockBlock() {
-    // Get the positions of the current block's tiles
-    std::vector<Position> tiles = currentBlock.GetCellPosition();
-
-    // Lock the block's positions into the grid by setting their state
+    std::vector<Position> tiles = currentBlock->GetCellPosition();
     for (const Position &item: tiles) {
-        if (!grid.IsCellOutside(item.row, item.column)) { // Ensure the cell is inside the grid
-            grid.gridPositions[item.row][item.column].state = currentBlock.id; // Set state to block ID
+        if (!grid.IsCellOutside(item.row, item.column)) {
+            grid.gridPositions[item.row][item.column].state = currentBlock->id;
         }
     }
 
-    // Transition to the next block
-    currentBlock = nextBlock;
-
-    // Check if the new block fits; if not, the game is over
-    if (!BlockFits()) {
-        gameOver = true;
-        currentBlock.id = 10; // Mark current block as "frozen" (or other appropriate logic)
-        currentBlock.Draw(11, 11); // Draw the block in the "frozen" state
-    }
-
-    // Update the score for placing a block
-    UpdateScore(0, 5);
-
-    // Get a new random block
+    currentBlock = std::move(nextBlock);
     nextBlock = GetRandomBlock();
 
-    // Clear any full rows and update the score accordingly
+    if (!BlockFits()) {
+        gameOver = true;
+        currentBlock->id = 10;
+        currentBlock->Draw(11, 11);
+    }
+
+    UpdateScore(0, 5);
     int rowsCleared = grid.ClearFullRows();
     UpdateScore(rowsCleared, 0);
 }
 
 bool Game::BlockFits() {
-    std::vector<Position> tiles = currentBlock.GetCellPosition();
+    std::vector<Position> tiles = currentBlock->GetCellPosition();
     for (Position item: tiles) {
         if (!grid.IsCellEmpty(item.row, item.column)) {
             return false;
@@ -161,7 +154,6 @@ bool Game::BlockFits() {
     }
     return true;
 }
-
 
 void Game::Reset() {
     grid.Initialize();
@@ -173,12 +165,9 @@ void Game::Reset() {
 }
 
 void Game::UpdateScore(int linesCleared, int moveDownPoints) {
-
     if (linesCleared > 0) {
         score += 10 * (linesCleared + 1) * (linesCleared + 1) * level;
     }
     level = score / 200 + 1;
     score += moveDownPoints;
-
-
 }
--- game.h
+++ game.h
@@ -2,7 +2,15 @@
 #include "grid.h"
 #include <vector>
 #include "block.h"
-
+#include <memory>
+class BlockBase;
+class LBlock;
+class JBlock;
+class IBlock;
+class OBlock;
+class SBlock;
+class TBlock;
+class ZBlock;
 class Game {
 public:
     Game();
@@ -19,8 +27,6 @@ protected:
 private:
     void MoveBlockLeft();
     void MoveBlockRight();
-    Block GetRandomBlock();
-    static std::vector<Block> GetAllBlocks();
     bool IsBlockOutside();
     void RotateBlock();
     void LockBlock();
@@ -28,7 +34,10 @@ private:
     void Reset();
     void UpdateScore(int linesCleared, int moveDownPoints);
 
-    std::vector<Block> blocks; // Collection of all block types
-    Block currentBlock;        // The block currently being moved
-    Block nextBlock;           // The next block to spawn
+    std::unique_ptr<BlockBase> GetRandomBlock();
+    static std::vector<std::unique_ptr<BlockBase>> GetAllBlocks();
+
+    std::vector<std::unique_ptr<BlockBase>> blocks;
+    std::unique_ptr<BlockBase> currentBlock;
+    std::unique_ptr<BlockBase> nextBlock;
 };
