650620b111634e430e1a5678d8941f16a2778438
agatabielska <agatabielska027@gmail.com>
2024-12-30 22:56:56 +0100

    delete comments

--- position.cpp
+++ position.cpp
@@ -1,7 +1,5 @@
 #include "position.h"
 
-// aggregation- Position is part of the Block class; Block doesn't own Position
-// composition- Position is part of the Grid class; Grid owns Position
 Position::Position(int row, int column, BlockType state) {
     this->row = row;
     this->column = column;
